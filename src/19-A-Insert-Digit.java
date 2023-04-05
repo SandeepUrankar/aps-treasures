9ee4dca153e4b50480ef667f5456b4023d018d89
SandeepUrankar <sandeepurankar.98@gmail.com>
2023-04-05 07:06:03 +0530

    Add Insert digit and find dup in array

---
+++ src/19-A-Insert-Digit.java
@@ -0,0 +1,33 @@
+// https://codeforces.com/contest/1811/problem/A
+import java.util.Scanner;
+class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        while(T --> 0){
+            int n = in.nextInt();
+            int d = in.nextInt();
+            Boolean done = false;
+            String input = in.next();
+            StringBuffer num = new StringBuffer(input);
+            for (int i = 0; i < num.length(); i++) {
+                if(Integer.parseInt(num.charAt(i)+"") < d){
+                    num.insert(i, d);
+                    System.out.println(num);
+                    done = true;
+                    break;
+                }else if(Integer.parseInt(num.charAt(i)+"") < d){
+                    num.insert(i -1, d);
+                    System.out.println(num);
+                    done = true;
+                    break;
+                }
+            }
+            if(!done){
+                num.append(d);
+                System.out.println(num);
+            }
+        }
+    }
+}
+
